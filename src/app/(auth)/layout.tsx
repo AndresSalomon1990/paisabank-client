de1d59caf7d846d257d72d0104c5d33c76104010
AndresSalomon1990 <a.salomon.1990@gmail.com>
2024-07-09 00:44:11 -0300

    chore: update AuthLayout html.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,11 +12,11 @@ interface Props {
 function AuthLayout({ children }: Readonly<Props>) {
   return (
     <main className="justify-betweeen flex h-dvh max-h-dvh w-screen flex-col items-center px-6 pb-9">
-      <header className="mb-10 mt-32 flex flex-col items-center justify-center text-center">
+      <div className="mb-10 mt-32 flex flex-col items-center justify-center text-center">
         <Image src={PaisaBankLogo} alt="logo" width={49} height={49} />
         <h1 className="text-4xl font-medium text-accent">PaisaBank</h1>
         <h4 className="text-subtitle-foreground">Comienza a manejar tu vida financiera</h4>
-      </header>
+      </div>
       {children}
     </main>
   );
