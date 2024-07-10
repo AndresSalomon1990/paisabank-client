5d028063727db8b5ea0bd880510bb7953e6b2cdb
AndresSalomon1990 <a.salomon.1990@gmail.com>
2024-07-10 20:29:28 -0300

    fix: update quick filters style's logic.

--- src/components/movements/MovementsFilters/QuickFilters/QuickFilters.tsx
+++ src/components/movements/MovementsFilters/QuickFilters/QuickFilters.tsx
@@ -1,55 +1,32 @@
 "use client";
 
 // External deps
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 // Internal deps
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { MOVEMENTS_TYPES } from "@/lib/enums";
-
-const options = [
-  {
-    id: 1,
-    value: "",
-    label: "Todos",
-  },
-  {
-    id: 2,
-    value: MOVEMENTS_TYPES.SUS,
-    label: "Débito aut.",
-  },
-  {
-    id: 3,
-    value: MOVEMENTS_TYPES.CASH_IN,
-    label: "Recibido",
-  },
-  {
-    id: 4,
-    value: MOVEMENTS_TYPES.CASH_OUT,
-    label: "Enviado",
-  },
-];
+import { QUICK_FILTER_OPTIONS } from "@/lib/constants";
 
 function QuickFilters() {
-  const [activeFilter, setActiveFilter] = useState(options[0]);
+  const [activeFilter, setActiveFilter] = useState(QUICK_FILTER_OPTIONS[0]);
   const { setValue } = useFormContext();
 
-  const handleClick = (option: (typeof options)[number]) => {
+  const handleClick = useCallback((option: (typeof QUICK_FILTER_OPTIONS)[number]) => {
     setActiveFilter(option);
     setValue("movementType", option.value);
-  };
+  }, []);
 
   return (
     <ScrollArea>
       <div className="flex w-full items-center gap-[10px]">
-        {options.map((option) => (
+        {QUICK_FILTER_OPTIONS.map((option) => (
           <Button
             key={option.id}
             variant="default"
             type="button"
-            className={`h-10 w-full rounded-2xl hover:bg-pillActive hover:text-pillActive-foreground bg-${activeFilter.id === option.id ? "pillActive" : "pillInactive"} px-5 text-${activeFilter.id === option.id ? "pillActive" : "pillInactive"}-foreground`}
+            className={`h-10 w-full rounded-2xl hover:bg-pillActive hover:text-pillActive-foreground ${activeFilter.id === option.id ? "bg-pillActive" : "bg-white"} px-5 ${activeFilter.id === option.id ? "text-pillActive-foreground" : "text-black"}`}
             onClick={() => handleClick(option)}
           >
             {option.label}
